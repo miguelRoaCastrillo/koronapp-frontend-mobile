e4c40296a49531266606638f184ec47f6eaca5db
Miguel Roa <miguelRoaCastrillo@gmail.com>
2021-04-06 23:30:20 -0400

    Se crea el TextField

--- src/scenes/GetStarted/View.js
+++ src/scenes/GetStarted/View.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import { View , Text } from "react-native";
+import { TextField } from "./../../components/TextField/TextField.js"
 import { PasswordField } from "./../../components/PasswordField/PasswordField.js";
 
 export default GetStartedView = () => {
 
     const [pass, setPass] = useState();
+    const [email, setEmail] = useState();
 
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
                 Get Started
             </Text>
-            <PasswordField label={'Contraseña'} placeholder={'Ingrese la contraseña'} onChangeText={text=>setPass(text)}/>            
+            <TextField label={'E-Mail'} placeholder={'Ingrese su E-Mail'} onChangeText={text=>setEmail(text)} />
+            <PasswordField label={'Contraseña'} placeholder={'Ingrese la contraseña'} onChangeText={text=>setPass(text)} />            
             <View>
+                <Text>
+                    {'El E-Mail es: ' + email}
+                </Text>
                 <Text>
                     {'La constraseña es:' + pass }
                 </Text>
