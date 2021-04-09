81dea1f40162de277b8f4050c4adee0d4e6b5633
Miguel Roa <miguelRoaCastrillo@gmail.com>
2021-04-08 22:21:37 -0400

    Definición de navegación de autenticación

--- src/scenes/GetStarted/View.js
+++ src/scenes/GetStarted/View.js
@@ -1,12 +1,39 @@
 import React from "react";
-import { View , Text } from "react-native";
+import { View , Text, TouchableOpacity, StyleSheet } from "react-native";
 
-export default GetStartedView = () => {
+
+export const GetStartedView = ({ navigation }) => {
+
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
                 Get Started
-            </Text>
+            </Text>         
+            <TouchableOpacity onPress={ () => navigation.navigate('Login') }>
+                <Text style={styles.button}>
+                    Go to Login
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={ () => navigation.navigate('SignUp') }>
+                <Text style={styles.button}>
+                    Go to SignUp
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+export const styles = StyleSheet.create({
+    button:{
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 20,
+        backgroundColor: '#3CDDDF',
+        color: 'white',
+        borderRadius: 10,
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+})
+
+export default GetStartedView;
