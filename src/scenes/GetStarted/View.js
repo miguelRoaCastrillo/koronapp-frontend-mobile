16b7de8a6248d7d14d20fb35c1bc43f772a36f28
Miguel Roa <miguelRoaCastrillo@gmail.com>
2021-04-09 13:47:26 -0400

    Creación y modificacion de textField

--- src/scenes/GetStarted/View.js
+++ src/scenes/GetStarted/View.js
@@ -1,28 +1,28 @@
 import React, { useState } from "react";
 import { View , Text } from "react-native";
-import  { PasswordField } from "./../../components/PasswordField/PasswordField.js";
-import { TextField } from "./../../components/TextField/TextField.js";
+import { TextField } from "./../../components/TextField/TextField.js"
+import { PasswordField } from "./../../components/PasswordField/PasswordField.js";
 
 export default GetStartedView = () => {
 
-    //Estado con todos los datos
-    const [data, setData] = useState(); 
     const [pass, setPass] = useState();
+    const [email, setEmail] = useState();
 
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>
-                Get Started                
-            </Text>            
-            <TextField label={ 'E-mail' } placeholder={ 'Ingrese su E-Mail' } onChangeText={ setData }/>            
-            <PasswordField label={ 'Contraseña' } placeholder={ 'Ingrese contraseña' } onChangeText={ setPass }/>            
-            <Text>
-                El E-Mail es: {data}                
+                Get Started
             </Text>
-            <Text>
-                La password es: {pass}
-            </Text>
-            <Text>Hola git</Text>
+            <TextField label={'E-Mail'} placeholder={'Ingrese su E-Mail'} onChangeText={text=>setEmail(text)} />
+            <PasswordField label={'Contraseña'} placeholder={'Ingrese la contraseña'} onChangeText={text=>setPass(text)} />            
+            <View>
+                <Text>
+                    {'El E-Mail es: ' + email}
+                </Text>
+                <Text>
+                    {'La constraseña es:' + pass }
+                </Text>
+            </View>
         </View>
     );
 }
